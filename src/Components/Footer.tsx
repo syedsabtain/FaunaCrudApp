cece585b341b51dec303ca9db6a118acbb4dc403
Syed <sabtain.info@gmail.com>
2020-11-08 03:08:50 -0800

    change a

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -13,24 +13,24 @@ const Footer=()=>{
                     <div className="col-md-12 ">
                         <div className=" flex-center">
 
-                            <a className="fb-ic">
+                            <span className="fb-ic">
                                 <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
-                            </a>
-                            <a className="tw-ic">
+                            </span>
+                            <span className="tw-ic">
                                 <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
-                            </a>
+                            </span>
                             <a className="gplus-ic icon-bg" target='blank' href='https://github.com/syedsabtain/Project12a'>
                                 <i className="fab fa-github fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
                             </a>
-                            <a className="li-ic">
+                            <span className="li-ic">
                                 <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
-                            </a>
-                            <a className="ins-ic">
+                            </span>
+                            <span className="ins-ic">
                                 <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
-                            </a>
-                            <a className="pin-ic">
+                            </span>
+                            <span className="pin-ic">
                                 <i className="fab fa-pinterest fa-lg white-text fa-2x"></i>
-                            </a>
+                            </span>
                         </div>
                     </div>
 
@@ -40,7 +40,7 @@ const Footer=()=>{
 
             <div className="footer-copyright text-center py-3">© 2020 Copyright:
                 <span>
-                     SyedSabtain</span>
+                    SyedSabtain</span>
             </div>
 
         </footer>
